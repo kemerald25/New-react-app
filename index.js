d4072028f54bf1824ec953a4206e07a1bf607641
Dev Royale <unityekeoba18@gmail.com>
2023-12-22 23:34:21 +0100

    updated

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-ReactDOM.render(<p>Hi, my name is Dev Royale</p>, document.getElementById("root"))
+const app =(
+    <div>
+        <h1>My Awesome React App</h1>
+        <h3>Why choose React</h3>
+        <ol>
+            <li>It is fun to work with</li>
+            <li>Its makes work easier</li>
+            <li>Coding feels alive</li>
+        </ol>
+    </div>
+)
+ReactDOM.render(app, document.getElementById("root"))
