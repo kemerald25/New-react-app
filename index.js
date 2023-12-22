c7439e9f3188c5884f5c25d728bf6e5046b669a3
Dev Royale <unityekeoba18@gmail.com>
2023-12-22 23:58:23 +0100

    updated

--- index.js
+++ index.js
@@ -14,17 +14,23 @@
 
 // Use pascal case instad of carmel case for naming react functions
 
-function AllApp () {
-    return(
-        <div>
-        <h1>My Awesome React App</h1>
-        <h3>Why choose React</h3>
-        <ol>
-            <li>It is fun to work with</li>
-            <li>Its makes work easier</li>
-            <li>Coding feels alive</li>
-        </ol>
-    </div>
+function AllApp() {
+    return (
+        <header>
+            <nav>
+                <h3>Dev Royale</h3>
+            </nav>
+            <div>
+                <h3>Why choose React</h3>
+                <ol>
+                    <li>It is fun to work with</li>
+                    <li>Its makes work easier</li>
+                    <li>Coding feels alive</li>
+                </ol>
+            </div>
+            <footer><p>@ 2023 Dev Royale All Right Reserved</p></footer>
+        </header>
+
     )
 }
 
