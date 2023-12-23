c5ff821fb88952ba589e56f7746e97209e2b83fd
Dev Royale <unityekeoba18@gmail.com>
2023-12-23 09:31:15 +0100

    updated

--- index.js
+++ index.js
@@ -13,24 +13,49 @@
 
 
 // Use pascal case instad of carmel case for naming react functions
+// import Header from './header'
 
-function AllApp() {
+
+function Header() {
+    return(
+        <header>
+            <nav>
+                <h3>Dev Royale</h3>
+                <ul>
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Footer() {
     return (
+        <small>@ 2023 Dev Royale All Right Reserved</small>
+    )
+}
+
+function MainContent() {
+    return(
         <div>
-            <header>
-                <nav>
-                    <h3>Dev Royale</h3>
-                </nav>
-            </header>
-            <div>
-                <h3>Why choose React</h3>
+            <h3>Why choose React</h3>
                 <ol>
                     <li>It is fun to work with</li>
                     <li>Its makes work easier</li>
                     <li>Coding feels alive</li>
                 </ol>
-            </div>
-            <footer><p>@ 2023 Dev Royale All Right Reserved</p></footer>
+        </div>
+    )
+}
+
+function AllApp() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
         </div>
 
     )
