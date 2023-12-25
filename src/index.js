f695eb08906dbeed0cd2c5fe6996c709beeefb74
Dev Royale <unityekeoba18@gmail.com>
2023-12-25 20:24:32 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+import React from 'react';
+
+
+// Use pascal case instad of carmel case for naming react functions
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import MainContent from '../components/MainContent'
+
+
+function App() {
+  return (
+      <div>
+          <Header />
+          <MainContent />
+          <Footer />
+      </div>
+
+  )
+}
+
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
